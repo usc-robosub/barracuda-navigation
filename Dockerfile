e25eb1a850e90db0fd7342d5e20de7ae7e9abaa4
Matthew Jiang <matthewyjiang@gmail.com>
2025-05-29 20:16:03 -0700

    Update Dockerfile and entrypoint for ROS workspace build

--- Dockerfile
+++ Dockerfile
@@ -16,5 +16,13 @@ COPY . /opt/barracuda-navigation
 # Set working directory
 WORKDIR /opt
 
+# Build the ROS workspace
+RUN /bin/bash -c "source /opt/ros/noetic/setup.bash && \
+    cd /opt/barracuda-navigation/catkin_ws && \
+    catkin_make && \
+    chmod +x /opt/barracuda-navigation/catkin_ws/src/rrt_star_planner/scripts/test_rrt_star_planner.py"
+
+WORKDIR /opt
+
 # Source the workspace on container start
 CMD ["/bin/bash", "/opt/barracuda-navigation/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-source /opt/ros/noetic/setup.bash
 
-# Build & Source catkin_ws
-cd barracuda-navigation/catkin_ws
-catkin_make
-source devel/setup.bash
+source /opt/ros/noetic/setup.bash
+source barracuda-navigation/catkin_ws/devel/setup.bash
 
 # Start the launch file (add --wait after other node starts roscore)
-roslaunch barracuda_navigation barracuda_navigation.launch 
-
+# roslaunch barracuda_navigation barracuda_navigation.launch
+roslaunch rrt_star_planner test_rrt_star_planner.launch
