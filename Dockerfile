41d6db36b73eaa76839adc910f4e1f35b5bb678a
Matthew Jiang <matthewyjiang@gmail.com>
2025-05-31 10:42:06 -0700

    updated submodule and entrypoint

--- Dockerfile
+++ Dockerfile
@@ -19,8 +19,7 @@ WORKDIR /opt
 # Build the ROS workspace
 RUN /bin/bash -c "source /opt/ros/noetic/setup.bash && \
     cd /opt/barracuda-navigation/catkin_ws && \
-    catkin_make && \
-    chmod +x /opt/barracuda-navigation/catkin_ws/src/rrt_star_planner/scripts/test_rrt_star_planner.py"
+    catkin_make 
 
 WORKDIR /opt
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 source /opt/ros/noetic/setup.bash
 source barracuda-navigation/catkin_ws/devel/setup.bash
 
+chmod +x /opt/barracuda-navigation/catkin_ws/src/rrt_star_planner/scripts/test_rrt_star_planner.py
+
 # Start the launch file (add --wait after other node starts roscore)
-# roslaunch barracuda_navigation barracuda_navigation.launch
-roslaunch rrt_star_planner test_rrt_star_planner.launch
+roslaunch barracuda_navigation barracuda_navigation.launch
