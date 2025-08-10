29532ed2648ccefbe0ab39d7ad4a70770654aebe
Matt Jiang <67533680+matthewyjiang@users.noreply.github.com>
2025-08-09 22:42:17 -0700

    Revert "3 interpolate waypoints path into differentiable function"

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,6 @@
 source /opt/ros/noetic/setup.bash
 source barracuda-navigation/catkin_ws/devel/setup.bash
 
-echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
-echo "source /opt/barracuda-navigation/catkin_ws/devel/setup.bash" >> ~/.bashrc
-
 chmod +x /opt/barracuda-navigation/catkin_ws/src/rrt_star_planner/scripts/test_rrt_star_planner.py
 
 # Start the launch file (add --wait after other node starts roscore)
