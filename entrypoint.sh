56735f29d41ab5a69d28b8972a334dd145640317
Matthew Jiang <matthewyjiang@gmail.com>
2025-08-11 02:46:05 -0700

    add wait

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,4 @@ source barracuda-navigation/catkin_ws/devel/setup.bash
 
 chmod +x /opt/barracuda-navigation/catkin_ws/src/rrt_star_planner/scripts/test_rrt_star_planner.py
 
-# Start the launch file (add --wait after other node starts roscore)
-roslaunch barracuda_navigation barracuda_navigation.launch
+roslaunch barracuda_navigation barracuda_navigation.launch --wait
